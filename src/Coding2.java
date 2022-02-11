d4085e568d4411fc7615022066b281a175b46287
lilitsaribekyan <lilit.saribekyan@picsart.com>
2022-02-11 22:05:42 +0400

    Initial commit

---
+++ src/Coding2.java
@@ -0,0 +1,18 @@
+public class Coding2 {
+    public static void main(String[] args) {
+//        byte a = 64, b;
+//        int i;
+//        i = a << 2;
+//        b = (byte) (a << 2);
+//
+//        System.out.println("Originl value  of a: " + a );
+//        System.out.println("i and b: " + i + " " + b );
+
+
+        boolean k;
+        int i = -1;
+
+        k = i <= 0 & i > 101 ? false : true;
+        System.out.println(k);
+    }
+}
---
+++ src/Lesson.java
@@ -0,0 +1,23 @@
+public class Lesson
+{
+    public static void main(String[] args) {
+        int a = 1+1;
+        int b = a*3;
+        int c = b/4;
+
+        a=c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        double da = 1+1;
+        double db = da*3;
+        double dc = b/4d;
+
+        System.out.println(da);
+        System.out.println(db);
+        System.out.println(dc);
+
+    }
+}
